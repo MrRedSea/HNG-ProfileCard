d12982572297b3d41b60006762786793a5a4f5be
Odogbo Moses <odogbodami25@gmail.com>
2025-10-20 11:12:43 +0100

    Update Script.js
    An update was made to the script.js file and also the Avatar upload functionality

--- script.js
+++ script.js
@@ -5,5 +5,22 @@ function updateUTCTime() {
 }
 
 updateUTCTime();
-setInterval(updateUTCTime, 1000); 
+setInterval(updateUTCTime, 1000);
 
+//Avatar upload functionality
+
+document.getElementById('upload-btn').addEventListener('click', function() {
+    document.getElementById('avatar-upload').click();
+});
+
+document.getElementById('avatar-upload').addEventListener('click', function(e) {
+    constfile = e.target.files[0];
+    if (files) {
+        const reader = new FileReader();
+        reader.onload =function(event) {
+            const avatar =document.querySelector('[data-testid="test-user-avatar]');
+            avatar.src = event.target.result;
+        }
+        reader.readAsDataURL(file);
+    }
+});
